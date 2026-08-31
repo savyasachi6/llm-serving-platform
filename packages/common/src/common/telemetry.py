b166524e8d047ca128aef9c20960b95f7dc278ae
Savyasachi Thati <savyasachi.thati@gmail.com>
2026-08-31 04:43:51 -0500

    fix(telemetry): fix structlog processor compatibility with PrintLoggerFactory and update dockerfile directory commands

--- packages/common/src/common/telemetry.py
+++ packages/common/src/common/telemetry.py
@@ -30,8 +30,7 @@ def add_context_vars(logger, method_name, event_dict):
 def setup_logging():
     structlog.configure(
         processors=[
-            structlog.stdlib.add_log_level,
-            structlog.stdlib.add_logger_name,
+            structlog.processors.add_log_level,
             add_context_vars,
             structlog.processors.TimeStamper(fmt="iso"),
             structlog.processors.JSONRenderer()
